a2b1819ed0adb8c03c3564f3c61299104c293af9
Ivan Vecerina <IvanR.vecerina@gmail.com>
2021-12-07 13:28:42 +0100

    Fixed bug on Config test file

--- src/Tests/ConfigTest.java
+++ src/Tests/ConfigTest.java
@@ -3,6 +3,7 @@ package Tests;
 import Config.ConfigManager;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ConfigTest {
     public static void main(String[] args) throws IOException {
@@ -11,6 +12,6 @@ public class ConfigTest {
         System.out.println(cfg.getServerAddress());
         System.out.println(cfg.getServerPort());
         System.out.println(cfg.getNbGroups());
-        System.out.println(cfg.getWitnessEmail());
+        System.out.println(Arrays.toString(cfg.getWitnessEmail()));
     }
 }
